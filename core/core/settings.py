3a18e5771ca206461271994543abaf323b3b338e
Tala <tala@MacBook-Air-Tala.local>
2025-06-20 16:48:23 +0600

    add

--- core/core/settings.py
+++ core/core/settings.py
@@ -63,14 +63,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bimar',
+#         'USER': 'navisdevs',
+#         'PASSWORD': 'adminadmin',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'business',
-        'USER': 'navisdevs',
-        'PASSWORD': 'adminadmin',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
