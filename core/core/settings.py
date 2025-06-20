cf09f8c0062ba74e7079cb2a01b4fb2deef60c5f
Tala <tala@MacBook-Air-Tala.local>
2025-06-20 17:26:34 +0600

    123

--- core/core/settings.py
+++ core/core/settings.py
@@ -63,22 +63,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'bimar',
-#         'USER': 'navisdevs',
-#         'PASSWORD': 'adminadmin',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bimar',
+        'USER': 'navisdevs',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
